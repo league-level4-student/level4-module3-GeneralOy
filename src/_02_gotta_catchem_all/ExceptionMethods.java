8647c60f9d6127ae10e0db927b5146f6522090eb
League Student <league@adminisorsiMac3.attlocal.net>
2019-07-11 19:32:21 -0700

    done
    moved on to mod4

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -9,4 +9,26 @@ public class ExceptionMethods {
 		}
 
 	}
+
+	public static String reverseString(String orgString) {
+		String reversing = orgString.trim();
+
+		// char[] revLetters;
+		if (reversing.isEmpty()) {
+			throw new IllegalStateException();
+		} else {
+			String reversedString = Character.toString(reversing.charAt(reversing.length() - 1));
+			/*
+			 * for (char letters : reversing.toCharArray()) {
+			 * reversedString.concat(Character.toString(letters)); }
+			 */
+			reversing = reversing.substring(0, reversing.length() - 1);
+			for (int i = reversing.length(); i > 0; i--) {
+				reversedString = reversedString.concat(reversing.substring(i - 1, i));
+				reversing = reversing.substring(0, (i));
+			}
+			return reversedString;
+		}
+
+	}
 }
